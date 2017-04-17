3f8fe5fca0e21e3fb66369bed3810f3cc7ad24cf
tabynar <qmflr79@naver.com>
2017-04-18 04:07:17 +0900

    bubble sort 단순 코드

---
+++ bubble sort/bubble sort.py
@@ -0,0 +1,15 @@
+c = int(input("num= "))
+a = []
+ca = c-1
+
+for i in range(c) :
+    a.append(int(input()))
+
+for i in range(c-1) :
+    for j in range(ca) :
+        if(a[j]>a[j+1]) :
+            a[j],a[j+1]=a[j+1],a[i]
+    ca -= 1
+
+for i in range(c) :
+    print(a[i])
